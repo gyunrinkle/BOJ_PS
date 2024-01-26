57a45bc7925607a555193b28bad1635bd964617b
Gyunseo Lee <rbstj0311@naver.com>
2024-01-26 17:23:26 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2236 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <queue>
+#include <tuple>
+#define endl '\n'
+using namespace std;
+const int MAX = 100;
+queue<pair<int, int>> q;
+string board[MAX];
+int is_visited[MAX][MAX], dist[MAX][MAX];
+int di[4] = {-1, 1, 0, 0};
+int dj[4] = {0, 0, -1, 1};
+int N, M;
+
+bool OOB(int i, int j) {
+  if (i < 0 || i >= N) return true;
+  if (j < 0 || j >= M) return true;
+  return false;
+}
+
+void ReadUserInput() {
+  cin >> N >> M;
+  for (int i = 0; i < N; i++) {
+    cin >> board[i];
+  }
+}
+
+void Solve() {
+  int cur_i, cur_j;
+  q.push({0, 0});
+  while(!q.empty()) {
+    tie(cur_i, cur_j) = q.front();
+    q.pop();
+    if (is_visited[cur_i][cur_j]) continue;
+    is_visited[cur_i][cur_j] = 1;
+    //cout << cur_i << " " << cur_j << "visited!" << endl;
+    if (cur_i == (N - 1) && cur_j == (M - 1)) {
+      //cout << cur_i << " " << cur_j << endl;
+      cout << dist[cur_i][cur_j] + 1 << endl;
+      return;
+    }
+    for (int k = 0; k < 4; k++) {
+      int next_i, next_j;
+      next_i = cur_i + di[k];
+      next_j = cur_j + dj[k];
+      if (OOB(next_i, next_j)) continue;
+      if (is_visited[next_i][next_j] == 1) continue;
+      if (board[next_i][next_j] == '0') continue;
+      if (dist[next_i][next_j] >= 1) continue;
+      dist[next_i][next_j] = dist[cur_i][cur_j] + 1;
+      q.push({next_i, next_j});
+    }
+  }
+}
+int main() {
+  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+  ReadUserInput();
+  Solve();
+  return 0;
+}
