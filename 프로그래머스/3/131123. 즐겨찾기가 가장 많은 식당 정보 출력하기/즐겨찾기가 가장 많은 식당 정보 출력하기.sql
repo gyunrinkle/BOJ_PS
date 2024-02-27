bb1941cc4dcce324554aa3231f48799e6accf431
Gyunseo Lee <rbstj0311@naver.com>
2024-02-27 20:02:30 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -0,0 +1,9 @@
+SELECT food_type, rest_id, rest_name, favorites
+FROM rest_info
+WHERE favorites in (
+    SELECT MAX(favorites)
+    FROM rest_info
+    GROUP BY food_type
+) 
+GROUP BY food_type
+ORDER BY food_type DESC;
